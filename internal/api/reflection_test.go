4549994365149c1643ad33f2d784a9d0638e028a
YASSERRMD <arafath.yasser@gmail.com>
2026-07-14 00:37:31 +0400

    api: fix variable shadowing in reflection_test.go

--- internal/api/reflection_test.go
+++ internal/api/reflection_test.go
@@ -7,16 +7,16 @@ import (
 
 func TestHandlerType(t *testing.T) {
 	h := &Handler{}
-	t := reflect.TypeOf(h).Elem()
-	if t.Name() != "Handler" {
-		t.Errorf("type name = %s, want Handler", t.Name())
+	typ := reflect.TypeOf(h).Elem()
+	if typ.Name() != "Handler" {
+		t.Errorf("type name = %s, want Handler", typ.Name())
 	}
 }
 
 func TestErrorResponseFields(t *testing.T) {
 	h := ErrorResponse{}
-	t := reflect.TypeOf(h)
-	if t.NumField() != 3 {
-		t.Errorf("ErrorResponse has %d fields, want 3", t.NumField())
+	typ := reflect.TypeOf(h)
+	if typ.NumField() != 3 {
+		t.Errorf("ErrorResponse has %d fields, want 3", typ.NumField())
 	}
 }
